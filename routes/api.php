ff60c21d29e163912aea1cd55b14c0107e3be274
oliver <oveler46@gmail.com>
2024-11-06 15:23:22 -0400

    configuracion correo

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,6 @@ Route::prefix('/v1/auth')->group(function(){
     });
 
 });
+
+Route::post('reset-password', [ResetPasswordController::class, "resetPassword"]);
+Route::post('change-password', [ResetPasswordController::class, "changePassword"]);
